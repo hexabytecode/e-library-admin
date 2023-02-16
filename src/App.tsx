8677f44918fb380ed8edbfbf252a89f6194b5f2a
Aditya Uphade <aditya.uphade@morningstar.com>
2023-02-16 20:09:44 +0530

    Updated Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Dashboard from "./Pages/Dashboard";
 
+// require('dotenv').config()
+
 export const App = () => (
   <ChakraProvider>
     <Router>
       <Routes>
-        <Route path="/login" element={ <Login /> } />
+        <Route path="/" element={ <Login /> } />
         <Route path="/dashboard" element={ <Dashboard /> } />
       </Routes>
     </Router>
--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -8,29 +8,51 @@ import {
   FormHelperText,
   Input,
 } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 export default function Form() {
   const navigate = useNavigate();
-  
+  let isError: boolean = false;
 
-  const handleSubmit = () => {
-    navigate("/dashboard");
-  }
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    // console.log("Email", event.target.email.value);
+    // console.log("Password", event.target.password.value);
+    if (
+      event.target.email.value === process.env.REACT_APP_USER_EMAIL &&
+      event.target.password.value === process.env.REACT_APP_PWD
+    ) {
+      navigate("/dashboard");
+    } else {
+      isError = true;
+    }
+  };
 
   return (
-    <Stack maxWidth={500} m="auto" spacing={4} mt={40}>
-      <FormControl>
-        <FormLabel>Email address</FormLabel>
-        <Input isRequired type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-      <FormControl>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" />
-        <FormHelperText>Password of your email.</FormHelperText>
-      </FormControl>
-      <Button size='lg' colorScheme={"blue"} onClick={handleSubmit}>Submit</Button>
-    </Stack>
+    <form onSubmit={handleSubmit}>
+      <Stack maxWidth={500} m="auto" spacing={6} mt={40}>
+        <FormControl>
+          <FormLabel>Email address</FormLabel>
+          <Input isRequired type="email" name="email" />
+          {!isError ? (
+            <FormHelperText>We'll never share your email.</FormHelperText>
+          ) : (
+            <FormErrorMessage>Incorrect Email</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input type="password" name="password" />
+          {!isError ? (
+            <FormHelperText>Password of your email.</FormHelperText>
+          ) : (
+            <FormErrorMessage></FormErrorMessage>
+          )}
+        </FormControl>
+        <Button size="lg" colorScheme={"blue"} type="submit">
+          Submit
+        </Button>
+      </Stack>
+    </form>
   );
 }
