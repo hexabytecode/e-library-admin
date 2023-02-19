468f214a589a7f06c06228ddb6bf81bf2153e7a8
Vishal Katkar <vishal.katkar@morningstar.com>
2023-02-19 21:40:07 +0530

    feat: Updated About us and ContactUs Content. - Vishal

--- src/Pages/Aboutus.tsx
+++ src/Pages/Aboutus.tsx
@@ -1,11 +1,42 @@
+import { VStack,Text, Box, Flex, Heading, Container } from '@chakra-ui/react'
 import React, { Component } from 'react'
 
 export default class Aboutus extends Component {
   render() {
     return (
-      <div>
-        About Us Page
-      </div>
+        <Container>
+      <Box>
+        <Flex>
+        <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
+        <Heading as="h2">What we write about</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
+          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
+          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
+          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
+          tortor, mattis nec lacus non, placerat congue elit.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
+          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
+          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
+          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
+          tortor, mattis nec lacus non, placerat congue elit.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
+          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
+          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
+          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
+          tortor, mattis nec lacus non, placerat congue elit.
+        </Text>
+      </VStack>
+      </Flex>   
+      </Box>
+      </Container>
     )
   }
 }
