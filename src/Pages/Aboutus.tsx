1c7c8e7bfe7ca90c3377f03834a30fc7adb46efb
Vishal Katkar <vishal.katkar@morningstar.com>
2023-02-22 00:07:07 +0530

    feat: Updated about us page. Vishal

--- src/Pages/Aboutus.tsx
+++ src/Pages/Aboutus.tsx
@@ -1,42 +1,149 @@
-import { VStack,Text, Box, Flex, Heading, Container } from '@chakra-ui/react'
-import React, { Component } from 'react'
+import { VStack,Text, Box, Flex, Heading, Container, Stack, Avatar, useColorModeValue } from '@chakra-ui/react'
+import React, { Component, ReactNode } from 'react'
+
+const Testimonial = ({ children }: { children: ReactNode }) => {
+  return <Box>{children}</Box>;
+};
+
+const TestimonialContent = ({ children }: { children: ReactNode }) => {
+  return (
+    <Stack
+      bg={useColorModeValue('white', 'gray.800')}
+      boxShadow={'lg'}
+      p={8}
+      rounded={'xl'}
+      align={'center'}
+      pos={'relative'}
+      _after={{
+        content: `""`,
+        w: 0,
+        h: 0,
+        borderLeft: 'solid transparent',
+        borderLeftWidth: 16,
+        borderRight: 'solid transparent',
+        borderRightWidth: 16,
+        borderTop: 'solid',
+        borderTopWidth: 16,
+        borderTopColor: useColorModeValue('white', 'gray.800'),
+        pos: 'absolute',
+        bottom: '-16px',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      }}>
+      {children}
+    </Stack>
+  );
+};
+
+const TestimonialHeading = ({ children }: { children: ReactNode }) => {
+  return (
+    <Heading as={'h3'} fontSize={'xl'}>
+      {children}
+    </Heading>
+  );
+};
+
+const TestimonialText = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text
+      textAlign={'center'}
+      color={useColorModeValue('gray.600', 'gray.400')}
+      fontSize={'sm'}>
+      {children}
+    </Text>
+  );
+};
+
+const TestimonialAvatar = ({
+  src,
+  name,
+  title,
+  size
+}: {
+  src: string;
+  name: string;
+  title: string;
+  size: string;
+}) => {
+  return (
+    <Flex align={'center'} mt={8} direction={'column'}>
+      <Avatar src={src} mb={2} size={size} />
+      <Stack spacing={-1} align={'center'}>
+        <Text fontWeight={600}>{name}</Text>
+        <Text fontSize={'sm'} color={useColorModeValue('gray.600', 'gray.400')}>
+          {title}
+        </Text>
+      </Stack>
+    </Flex>
+  );
+};
 
 export default class Aboutus extends Component {
   render() {
     return (
-        <Container>
-      <Box>
-        <Flex>
-        <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
-        <Heading as="h2">What we write about</Heading>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
-          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
-          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
-          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
-          tortor, mattis nec lacus non, placerat congue elit.
-        </Text>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
-          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
-          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
-          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
-          tortor, mattis nec lacus non, placerat congue elit.
-        </Text>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
-          pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
-          imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
-          sapien. Suspendisse placerat vulputate posuere. Curabitur neque
-          tortor, mattis nec lacus non, placerat congue elit.
-        </Text>
-      </VStack>
-      </Flex>   
-      </Box>
+      <Box bg={'gray.200'}>
+      <Container maxW={'7xl'} py={16} as={Stack} spacing={12}>
+        <Stack spacing={0} align={'center'}>
+          <Heading>Our Clients Speak</Heading>
+          <Text>We have been working with clients around the world</Text>
+        </Stack>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          spacing={{ base: 10, md: 4, lg: 10 }}>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Efficient Collaborating</TestimonialHeading>
+              <TestimonialText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
+                neque sed imperdiet nibh lectus feugiat nunc sem.
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                '../JackMa.jpg'
+              }
+              name={'Jack Ma'}
+              title={'CEO at Alibaba'}                  
+              size={'lg'}                 
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Intuitive Design</TestimonialHeading>
+              <TestimonialText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
+                neque sed imperdiet nibh lectus feugiat nunc sem.
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                '../jeff-bezos.jpeg'
+              }
+              name={'Jeff Bezos'}
+              title={'CEO at Amazon'}
+              size={'lg'} 
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Mindblowing Service</TestimonialHeading>
+              <TestimonialText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
+                neque sed imperdiet nibh lectus feugiat nunc sem.
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                '../mark-zuckerberg.jpg'
+              }
+              name={'Mark Zuckerberg'}
+              title={'CEO at Facebook'}
+              size={'lg'} 
+            />
+          </Testimonial>
+        </Stack>
       </Container>
+    </Box>
     )
   }
 }
